0bcb8da9baae78e9326fd13069f6bcd9c00e1631
s_elmas <stakasaki707@gmail.com>
2021-03-07 22:11:01 +0900

    Initial commit

---
+++ app/src/main/java/com/example/stakasaki/coffeeorder/registerlogin/RegisterLoginActivity.kt
@@ -0,0 +1,28 @@
+package com.example.stakasaki.coffeeorder.registerlogin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.stakasaki.coffeeorder.R
+
+/**
+ * Activity for register and login
+ */
+
+class RegisterLoginActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // フラグメントごとにタイトルを設定する
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_register_login) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+}
